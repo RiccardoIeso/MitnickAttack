4900a0e37122518e7ad02ce02fbda067eceebd9b
rick <riccardo.iesari@gmail.com>
2020-09-14 23:27:12 +0200

    error on pcapnext

--- packetsniffer.c
+++ packetsniffer.c
@@ -3,7 +3,43 @@
 #include <libnet.h>
 #include <stdlib.h>
 #include "sender.h"
-#include "headerdefinition.h"
+/* IP header */
+struct ipHeader {
+    u_char ip_vhl;        /* version << 4 | header length >> 2 */
+    u_char ip_tos;        /* type of service */
+    u_short ip_len;        /* total length */
+    u_short ip_id;        /* identification */
+    u_short ip_off;        /* fragment offset field */
+    u_char ip_ttl;        /* time to live */
+    u_char ip_p;        /* protocol */
+    u_short ip_sum;        /* checksum */
+    struct in_addr ip_src, ip_dst; /* source and dest address */
+};
+
+
+#define IP_HEADER_LENGTH(ip)    (((ip)->ip_vhl) & 0x0f)
+
+struct tcpHeader {
+    u_short th_sport;    /* source port */
+    u_short th_dport;    /* destination port */
+    u_int th_seq;        /* sequence number */
+    u_int th_ack;        /* acknowledgement number */
+    u_char th_offx2;    /* data offset, rsvd */
+    #define TH_OFF(th)    (((th)->th_offx2 & 0xf0) >> 4)
+    u_char th_flags;
+    #define TH_FIN 0x01
+    #define TH_SYN 0x02
+    #define TH_RST 0x04
+    #define TH_PUSH 0x08
+    #define TH_ACK 0x10
+    #define TH_URG 0x20
+    #define TH_ECE 0x40
+    #define TH_CWR 0x80
+    #define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+    u_short th_win;        /* window */
+    u_short th_sum;        /* checksum */
+    u_short th_urp;        /* urgent pointer */
+};
 void packetSnifferInitialize(libnet_t *l,u_long kevin, u_long xterminal)
 {
     char *dev;
@@ -14,7 +50,6 @@ void packetSnifferInitialize(libnet_t *l,u_long kevin, u_long xterminal)
     struct bpf_program fp;         
     struct pcap_pkthdr header;
     const u_char *packet;	
-    tcpH *stcp;
     dev = pcap_lookupdev(errbuff);
 
     //Check on device
@@ -62,8 +97,10 @@ void packetSnifferInitialize(libnet_t *l,u_long kevin, u_long xterminal)
             printf("\n%s", pcap_geterr(des));
             //exit(0);
         }
-        const struct tcp_header *tcp = (struct tcpHeader *) (packet + 14 + sizeof(struct ipHeader));
-        uint32_t seq = ntohl(stcp->th_seq);
+        const struct ipHeader *ip = (struct ipHeader *) (packet + 14);
+        u_int size_ip = IP_HEADER_LENGTH(ip) * 4;
+        const struct tcpHeader *tcp = (struct tcpHeader *) (packet + 14 + size_ip);
+        u_int32_t seq = ntohl(tcp->th_seq);
         printf("\n seq: %d", seq);
         fflush(stdout);
         printf("miao");
