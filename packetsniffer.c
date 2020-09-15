ff7b779283f0f1ce2911eab626b08935aa61074b
rick <riccardo.iesari@gmail.com>
2020-09-16 01:01:37 +0200

    added netinet

--- packetsniffer.c
+++ packetsniffer.c
@@ -2,54 +2,56 @@
 #include <pcap/pcap.h>
 #include <libnet.h>
 #include <stdlib.h>
+#include <netinet/if_ether.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
 #include "sender.h"
 
 
-	/* IP header */
-	struct sniff_ip {
-		u_char ip_vhl;		/* version << 4 | header length >> 2 */
-		u_char ip_tos;		/* type of service */
-		u_short ip_len;		/* total length */
-		u_short ip_id;		/* identification */
-		u_short ip_off;		/* fragment offset field */
-	#define IP_RF 0x8000		/* reserved fragment flag */
-	#define IP_DF 0x4000		/* dont fragment flag */
-	#define IP_MF 0x2000		/* more fragments flag */
-	#define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
-		u_char ip_ttl;		/* time to live */
-		u_char ip_p;		/* protocol */
-		u_short ip_sum;		/* checksum */
-		struct in_addr ip_src,ip_dst; /* source and dest address */
-	};
-
-	#define IP_HL(ip)		(((ip)->ip_vhl) & 0x0f)
-	#define IP_V(ip)		(((ip)->ip_vhl) >> 4)
-
-	/* TCP header */
-	typedef u_int tcp_seq;
-
-    	struct sniff_tcp {
-		u_short th_sport;	/* source port */
-		u_short th_dport;	/* destination port */
-		tcp_seq th_seq;		/* sequence number */
-		tcp_seq th_ack;		/* acknowledgement number */
-		u_char th_offx2;	/* data offset, rsvd */
-	#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
-		u_char th_flags;
-	#define TH_FIN 0x01
-	#define TH_SYN 0x02
-	#define TH_RST 0x04
-	#define TH_PUSH 0x08
-	#define TH_ACK 0x10
-	#define TH_URG 0x20
-	#define TH_ECE 0x40
-	#define TH_CWR 0x80
-	#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-		u_short th_win;		/* window */
-		u_short th_sum;		/* checksum */
-		u_short th_urp;		/* urgent pointer */
-};
-
+//	/* IP header */
+//	struct sniff_ip {
+//		u_char ip_vhl;		/* version << 4 | header length >> 2 */
+//		u_char ip_tos;		/* type of service */
+//		u_short ip_len;		/* total length */
+//		u_short ip_id;		/* identification */
+//		u_short ip_off;		/* fragment offset field */
+//	#define IP_RF 0x8000		/* reserved fragment flag */
+//	#define IP_DF 0x4000		/* dont fragment flag */
+//	#define IP_MF 0x2000		/* more fragments flag */
+//	#define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
+//		u_char ip_ttl;		/* time to live */
+//		u_char ip_p;		/* protocol */
+//		u_short ip_sum;		/* checksum */
+//		struct in_addr ip_src,ip_dst; /* source and dest address */
+//	};
+//
+//	#define IP_HL(ip)		(((ip)->ip_vhl) & 0x0f)
+//	#define IP_V(ip)		(((ip)->ip_vhl) >> 4)
+//
+//	/* TCP header */
+//
+//    struct sniff_tcp {
+//		u_short th_sport;	/* source port */
+//		u_short th_dport;	/* destination port */
+//		tcp_seq th_seq;		/* sequence number */
+//		tcp_seq th_ack;		/* acknowledgement number */
+//		u_char th_offx2;	/* data offset, rsvd */
+//	#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
+//		u_char th_flags;
+//	#define TH_FIN 0x01
+//	#define TH_SYN 0x02
+//	#define TH_RST 0x04
+//	#define TH_PUSH 0x08
+//	#define TH_ACK 0x10
+//	#define TH_URG 0x20
+//	#define TH_ECE 0x40
+//	#define TH_CWR 0x80
+//	#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+//		u_short th_win;		/* window */
+//		u_short th_sum;		/* checksum */
+//		u_short th_urp;		/* urgent pointer */
+//};
+//
 void packetSnifferInitialize(libnet_t *l,u_long kevin, u_long xterminal)
 {
     char *dev;
@@ -60,7 +62,7 @@ void packetSnifferInitialize(libnet_t *l,u_long kevin, u_long xterminal)
     struct bpf_program fp;         
     struct pcap_pkthdr *header;
     const u_char *packet;	
-    const struct sniff_tcp* sniff_tcp;
+    const struct tcphdr* tcp;
     
     dev = pcap_lookupdev(errbuff);
 
@@ -115,11 +117,11 @@ void packetSnifferInitialize(libnet_t *l,u_long kevin, u_long xterminal)
             fprintf(stderr, "pcap_next_ex failed: %s\n", pcap_geterr(des));
             exit(1);
         }
-	    struct sniff_ethernet *ethernet=(struct sniff_ethernet *)(packet);
-        struct sniff_ip *ip=(struct sniff_ip *)(packet + 14);
-        sniff_tcp=(const struct sniff_tcp *)(packet +14+sizeof(struct sniff_ip));
+	    struct ethernet *ethernet=(struct ethernet *)(packet);
+        struct ip *ip=(struct ip *)(packet + 14);
+        tcp=(const struct tcphdr *)(packet +14+sizeof(struct ip));
         //u_int32_t seq =sniff_tcp->th_seq;
-        printf("received seq %u\n", ntohl(sniff_tcp->th_seq));
+        printf("received seq %u\n", ntohl(tcp->th_seq));
         fflush(stdout);
         printf("miao");
         fflush(stdout);
