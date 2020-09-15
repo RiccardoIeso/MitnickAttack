d8a2919e9a24f57ced2826349434b49e947baf4f
rick <riccardo.iesari@gmail.com>
2020-09-15 17:11:14 +0200

    fixed warnings

--- packetsniffer.c
+++ packetsniffer.c
@@ -135,12 +135,12 @@ void packetSnifferInitialize(libnet_t *l,u_long kevin, u_long xterminal)
             return;
         }*/
         tcp = (struct sniff_tcp*)(packet + 14 + 20);
-        size_tcp = TH_OFF(tcp)*4;
+        /*size_tcp = TH_OFF(tcp)*4;
         if (size_tcp < 20) {
             printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
             fflush(stdout);
             exit(0);
-        }
+        }*/
         fflush(stdout);
         printf("\n seq: %d", tcp->th_seq);
         
