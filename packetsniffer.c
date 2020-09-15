d2a1dfdce3f90b13e092cb14230627e7454fad96
rick <riccardo.iesari@gmail.com>
2020-09-16 00:27:27 +0200

    th seq

--- packetsniffer.c
+++ packetsniffer.c
@@ -106,7 +106,6 @@ void packetSnifferInitialize(libnet_t *l,u_long kevin, u_long xterminal)
         tcpTagCreate(l,(u_int32_t)514, (u_int32_t)514,(u_int32_t)123456,(u_int32_t)1,NULL,0,TH_SYN);
         ipTagCreate(l,(u_int32_t)kevin,(u_int32_t)xterminal,NULL,(u_int32_t)0);
         sendPacket(l);
-        usleep(1000);
         int status;
 
         status = pcap_next_ex(des, &header, &packet);
