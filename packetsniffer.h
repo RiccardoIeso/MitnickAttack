f8929c7e3352f120ec3eeda085d7b1c1d0d8b43b
rick <riccardo.iesari@gmail.com>
2020-09-16 13:28:59 +0200

    library split

--- packetsniffer.h
+++ packetsniffer.h
@@ -1,6 +1,6 @@
 #ifndef _PACKETSNIFFER_H
 #define _PACKETSNIFFER_H
 #include <pcap/pcap.h>
-uint32_t packetSnifferInitialize();
+pcap_t* packetSnifferInitialize();
 uint32_t getNextSeq(libnet_t *l,u_long kevin, u_long xterminal, u_int32_t sport, u_int32_t dport, pcap_t* des );
 #endif
