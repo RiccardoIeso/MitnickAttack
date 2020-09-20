fa2630f60de28f565bfa36336b93852c285d14e1
rick <riccardo.iesari@gmail.com>
2020-09-20 11:26:31 +0200

    flood changed

--- MitnickAttack.c
+++ MitnickAttack.c
@@ -71,7 +71,7 @@ int main(int argc, char **argv)
     printf("\nDisabling server...");
     fflush(stdout);
     disableServer(l,kevin,server);
-    return 0;
+    exit(0);
     //Create the pscket sniffer
     pcap_t *des=packetSnifferInitialize();
    // double tta= timeToAnswer(l,kevin,xterminal,514,514,des);
